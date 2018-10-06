9e0c308d874d57b2979af20845c6ee9811349c54
ethanryan <ethanryan@gmail.com>
2018-10-06 17:51:16 -0400

    passing grid as props from GameContainer to GameGrid

--- src/components/GameGrid.js
+++ src/components/GameGrid.js
@@ -2,25 +2,8 @@ import React, { Component } from 'react';
 
 import Cell from './Cell';
 
-let grid = [] //game grid is an array of cells
-//NOTE: is grid an array of arrays, or an array of objects??
-
-let gridLength = 25
-
-for (var i=0; i < gridLength; i++) {
-  let cell = {id: '', alive: ''} //this needs to be declared in the for loop...
-  let randomValue = (Math.random() > .5) ? true : false //this needs to be declared in the for loop...
-  console.log('i is: ', i)
-  cell.id = i
-  cell.alive = randomValue
-  console.log('cell now is: ', cell)
-  grid.push(cell)
-}
-
-console.log('grid is: ', grid)
 
 class GameGrid extends Component {
-
 
   render() {
     return (
@@ -32,7 +15,7 @@ class GameGrid extends Component {
         game grid...
 
         <div className="grid-container">
-          {grid.map(eachCell => {
+          {this.props.grid.map(eachCell => {
             console.log('eachCell is: ', eachCell)
             return (
               <div key={eachCell.id} className="grid-item">
--- src/container/GameContainer.js
+++ src/container/GameContainer.js
@@ -4,12 +4,33 @@ import GameGrid from '../components/GameGrid';
 import Rules from '../components/Rules'; //NOTE: delete this component later...
 import ControlPanel from '../components/ControlPanel';
 
+
+let grid = [] //game grid is an array of cells
+//NOTE: is grid an array of arrays, or an array of objects??
+
+let gridLength = 25
+
+for (var i=0; i < gridLength; i++) {
+  let cell = {id: '', alive: ''} //this needs to be declared in the for loop...
+  let randomValue = (Math.random() > .5) ? true : false //this needs to be declared in the for loop...
+  console.log('i is: ', i)
+  cell.id = i
+  cell.alive = randomValue
+  console.log('cell now is: ', cell)
+  grid.push(cell)
+}
+
+console.log('grid is: ', grid)
+
+
 class GameContainer extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
       message: "hello world",
+      generation: 0,
+      // grid: grid,
     }
   }
 
@@ -28,6 +49,7 @@ class GameContainer extends Component {
         </div>
 
         <GameGrid
+          grid={grid}
         />
 
         <ControlPanel

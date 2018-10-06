d4220ed7a95c631000bc94b181e49cb2a570398b
ethanryan <ethanryan@gmail.com>
2018-10-06 14:05:10 -0400

    added GameContainer, rendered by App

---
+++ src/container/GameContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameContainer extends Component {
+  render() {
+    return (
+      <div className="GameContainer">
+        Game Container here!!!
+      </div>
+    );
+  }
+}
+
+export default GameContainer;

546796f979ef40168bf23d9b3b16aea04e70f335
ethanryan <ethanryan@gmail.com>
2018-10-06 14:11:06 -0400

    added GameGrid component, rendered by GameContainer

---
+++ src/components/GameGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class GameGrid extends Component {
+  render() {
+    return (
+      <div className="GameGrid">
+        Game Grid here!!!
+      </div>
+    );
+  }
+}
+
+export default GameGrid;
--- src/container/GameContainer.js
+++ src/container/GameContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import GameGrid from '../components/GameGrid';
+
 class GameContainer extends Component {
   render() {
     return (
       <div className="GameContainer">
         Game Container here!!!
+
+        <GameGrid
+        />
+        
       </div>
     );
   }

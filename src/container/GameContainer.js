25b41ad2af20a08166a8f312a4f93b1414acb755
ethanryan <ethanryan@gmail.com>
2018-10-06 15:30:46 -0400

    adding state to GameContainer

--- src/container/GameContainer.js
+++ src/container/GameContainer.js
@@ -5,7 +5,16 @@ import Rules from '../components/Rules'; //NOTE: delete this component later...
 import ControlPanel from '../components/ControlPanel';
 
 class GameContainer extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      message: "hello world",
+    }
+  }
+
   render() {
+    console.log('GameContainer, this.state is: ', this.state)
     return (
       <div className="GameContainer center-block">
 

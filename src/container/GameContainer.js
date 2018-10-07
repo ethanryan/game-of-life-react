60323247a7d2ba556cee565ef6c809147b55d298
ethanryan <ethanryan@gmail.com>
2018-10-06 20:28:09 -0400

    updated GameContainer with makeMatrix function, now storing x and y coordinates in each cell

--- src/container/GameContainer.js
+++ src/container/GameContainer.js
@@ -5,22 +5,51 @@ import Rules from '../components/Rules'; //NOTE: delete this component later...
 import ControlPanel from '../components/ControlPanel';
 
 
-let grid = [] //game grid is an array of cells
-//NOTE: is grid an array of arrays, or an array of objects??
 
-let gridLength = 25
 
-for (var i=0; i < gridLength; i++) {
-  let cell = {id: '', alive: ''} //this needs to be declared in the for loop...
-  let randomValue = (Math.random() > .5) ? true : false //this needs to be declared in the for loop...
-  // console.log('i is: ', i)
-  cell.id = i
-  cell.alive = randomValue
-  // console.log('cell now is: ', cell)
-  grid.push(cell)
-}
 
-console.log('grid is: ', grid)
+////NOTE: trying this, got online:
+// GridCell.prototype.countLiveNeighbors = function(grid) {
+//     var liveCount = 0;
+//
+//     // Count neighbors
+//     for (var i = -1; i < 2; ++i) {
+//         for (var j = -1; j < 2; ++j) {
+//             var dx = (this.x + i) % grid.width;
+//             var dy = (this.y + j) % grid.height;
+//
+//             var cell = grid.getCell(dx, dy);
+//             if (cell && cell != this && cell.isAlive()) {
+//                 ++liveCount;
+//             }
+//         }
+//     }
+//
+//     return liveCount;
+// };
+
+function countLiveNeighbors(grid) {
+  console.log('countLiveNeighbors called with grid: ', grid)
+    var liveCount = 0;
+
+    // Count neighbors
+    for (var i = -1; i < 2; ++i) {
+        for (var j = -1; j < 2; ++j) {
+            var dx = (this.x + i) % grid.width;
+            var dy = (this.y + j) % grid.height;
+
+            var cell = grid.getCell(dx, dy);
+            if (cell && cell !== this && cell.isAlive()) {
+                ++liveCount;
+            }
+        }
+    }
+
+    return liveCount;
+};
+
+// countLiveNeighbors(grid)
+
 
 
 class GameContainer extends Component {
@@ -30,15 +59,81 @@ class GameContainer extends Component {
     this.state = {
       message: "hello world",
       generation: 0,
-      // grid: grid,
+      grid: [], //initial state of grid is an empty array...
+      cell: {
+        id: '',
+        alive: '',
+        row: '',
+        column: '',
+        liveNeighors: '',
+        generation: ''
+      }
     }
     this.addGeneration = this.addGeneration.bind(this)
+    this.getInitialGrid = this.getInitialGrid.bind(this)
   }
+
+  makeMatrix = (x, y) => (
+    Array.from(
+      { length: x }, (thing, x) => Array.from(
+        { length: y }, (thing, y) => ({ x, y })
+      )
+    )
+  );
+  // console.log(makeMatrix(2, 2));
+
+  getInitialGrid() {
+    let grid = [] //game grid is an array of cells
+    //NOTE: is grid an array of arrays, or an array of objects??
+
+    let numberOfRows = 5
+    let numberOfColumns = 5
+
+    //const matrix = new Array(numberOfRows).fill(0).map(() => new Array(numberOfColumns).fill(1, 0, 5));
+    let matrix = this.makeMatrix(numberOfRows, numberOfColumns)
+
+    console.log('matrix is: ', matrix)
+    let matrixFlat = matrix.flat()
+    console.log('matrixFlat is: ', matrixFlat)
+
+    let gridLength = numberOfRows * numberOfColumns
+    console.log('gridLength is: ', gridLength)
+
+    for (var i=0; i < gridLength; i++) {
+      console.log('matrixFlat[i] is: ', matrixFlat[i])
+      // let cell = {id: '', alive: ''} //this needs to be declared in the for loop...
+      let cell = {
+        id: '',
+        alive: '',
+        x: '', //aka row
+        y: '', //aka column
+        liveNeighors: '', //make this a function...
+        generation: ''
+      }
+      let randomValue = (Math.random() > .5) ? true : false //this needs to be declared in the for loop...
+      // console.log('i is: ', i)
+      cell.id = i
+      cell.alive = randomValue
+      cell.x = matrixFlat[i].x
+      cell.y = matrixFlat[i].y
+      // console.log('cell now is: ', cell)
+      grid.push(cell)
+    }
+
+    console.log('getInitialGrid - grid is: ', grid)
+    this.setState({grid: grid})
+    return grid
+  }
+  // getInitialGrid()
 
   addGeneration() {
     let currentGeneration = this.state.generation
     let nextGeneration = currentGeneration + 1
     this.setState({generation: nextGeneration})
+  }
+
+  componentDidMount() {
+    this.getInitialGrid()
   }
 
   render() {
@@ -56,7 +151,7 @@ class GameContainer extends Component {
         </div>
 
         <GameGrid
-          grid={grid}
+          grid={this.state.grid} //passing grid from state...
         />
 
         <button className="center-text" onClick={this.addGeneration}>

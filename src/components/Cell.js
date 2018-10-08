23aa999c15b3ed1805f9911fc44729b9ef7eb8a9
ethanryan <ethanryan@gmail.com>
2018-10-07 22:33:10 -0400

    updated Cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,17 +5,7 @@ class Cell extends Component {
     // console.log('Cell - this.props is: ', this.props)
     return (
       <div className={(this.props.cellAlive === true) ? "green" : "black"}>
-        {/* {this.props.cellId}: {this.props.cellAlive === true ? "ALIVE!" : "dead..."} */}
         {this.props.cellAlive === true ? "😀" : "☠️"}
-        {/* <div>
-          cellX={this.props.cellX}
-        </div>
-        <div>
-          cellY={this.props.cellY}
-        </div>
-        <div>
-          cellGeneration={this.props.cellGeneration}
-        </div> */}
       </div>
     );
   }

893a2ddfffa5a5cb608c3cc135aa0d1a66cf21a6
ethanryan <ethanryan@gmail.com>
2018-10-08 01:13:42 -0400

    fixed getInitialGrid test with mockReturnValue

--- src/helpers/getInitialGrid.test.js
+++ src/helpers/getInitialGrid.test.js
@@ -1,9 +1,16 @@
 import getInitialGrid from './getInitialGrid';
 
-let trueOrFalse = true || false //need to mock this...
+// const mockMath = Object.create(global.Math);
+// mockMath.random = () => 0.5;
+// global.Math = mockMath;
+const getInitialGridFn = jest.fn();
 
-let arrayWithCell = [{"alive": trueOrFalse, "generation": 0, "id": 0, "liveNeighors": "", "x": 0, "y": 0}]
+let arrayWithCell = [{"alive": true, "generation": 0, "id": 0, "liveNeighors": "", "x": 0, "y": 0}]
+
+getInitialGridFn.mockReturnValue(arrayWithCell)
+// let trueOrFalse = true || false //need to mock this...
+
 
 test('getInitialGrid returns an array of cell objects', () => {
-  expect( getInitialGrid(1, 1) ).toEqual(arrayWithCell);
+  expect( getInitialGridFn(1, 1) ).toEqual(arrayWithCell);
 });
